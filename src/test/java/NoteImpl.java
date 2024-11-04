fe81f50900b72de3bae0f2687ab6939af8b1bcf0
Olly <olly.aaa@mail.ru>
2024-11-04 15:50:09 +0500

    добавление сетеров и гетеров

--- src/test/java/NoteImpl.java
+++ src/test/java/NoteImpl.java
@@ -1,3 +1,29 @@
 public class NoteImpl {
+    private String text;
+    private String date;
+
+    public NoteImpl(String text, String date) {
+        this.text = text;
+        this.date = date;
+    }
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String getDate() {
+        return date;
+    }
+
+    @Override
+    public void setDate(String date) {
+        this.date = date;
+    }
 
 }
